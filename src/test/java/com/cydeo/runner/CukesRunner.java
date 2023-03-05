034647c71850327c36d4502ecd857311911a0bb1
Gul <gulderendav@gmail.com>
2023-03-04 18:53:50 -0800

    Initial commit

---
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.cydeo.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                
+        }
+
+
+
+)
+
+public class CukesRunner {
+
+}
